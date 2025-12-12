8853a964c97734702b173bb2acd5a1087cf557c0
eyelady <keeganpatton@gmail.com>
2025-12-11 20:19:24 -0600

    yt embed boiler

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import InstaViewer from "@/components/igViewer";
+import YoutubeEmbed from "@/components/YoutubeEmbed";
 
 export default function Home() {
   return (
@@ -6,6 +7,11 @@ export default function Home() {
 
       <div> EYELADY </div>
       <InstaViewer />
+      <iframe
+        src="https://youtu.be/fUikjgcU45o?si=uje_uRTm1hFCeFez"
+      />
+
+      <YoutubeEmbed embedId='<iframe width="560" height="315" src="https://www.youtube.com/embed/fUikjgcU45o?si=uje_uRTm1hFCeFez" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>' />
 
     </div>
   );
