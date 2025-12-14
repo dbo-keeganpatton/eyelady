fa960bb50b80ef6ef6b1414feff8e4e8650aaa38
eyelady <keeganpatton@gmail.com>
2025-12-14 17:13:34 -0600

    update readme

---
+++ components/frontPageVideo.tsx
@@ -0,0 +1,19 @@
+// This should always be our most recent video, just change src arg 
+// if we drop something new
+
+export default function FrontPageVideo() {
+  return (
+    <>
+      <iframe
+        className="w-full h-full p-20"
+        src="https://www.youtube.com/embed/fUikjgcU45o?si=ckx56lGe-pOtgDKK"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+
+    </>
+
+  )
+}
