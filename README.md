41c674f2d7b49d79f3bfb871d827be8783baaae1
Keegan Patton <120262579+dbo-keeganpatton@users.noreply.github.com>
2025-12-14 17:19:54 -0600

    Myspace theme (#3)
    * Add assets | stage style
    
    * update theme to cyan | add eyeladytext comp
    
    * Correct grid issues with image comp
    
    * update readme
    
    * add front page video

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 ## EYELADY
 
 # EYELADY
+
+# Contribution
+
+1. Clone Repo locally [`git clone`]
+   - `git@github.com:dbo-keeganpatton/eyelady.git` for SSH
+   - `https://github.com/dbo-keeganpatton/eyelady.git` for http
+2. Create feature branch `git checkout -b [your-branch]`
+3. Add code changes locally. - `git add .` - `git commit -m 'here is what I did'`
+4. Sync with main branch before pushing back to remote.
+   - `git pull origin main`
+5. Push to this repo with `git push -u origin [your-branch]`
+6. Open a Pull request from your branch to main
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,64 @@
 import InstaViewer from "@/components/igViewer";
+import EyeLadyText from "@/components/eyeladyText";
+import FrontPageVideo from "@/components/frontPageVideo";
+import Image from "next/image";
 import Link from "next/link";
+
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
+    <div className="flex flex-col w-screen">
 
-      <div> EYELADY </div>
 
-      <Link href="/video" className="p-1 border rounded-sm hover:bg-green-600/40 hover:scale-110 transition-transform duration-300"> <button> Videos </button> </Link>
+      {/* Top Banner BEGIN */}
+      <div className="w-full px-2 pt-2">
+        <div className="grid grid-cols-3 items-center border border-cyan-500 text-md p-1 m-2 rounded-md shadow-lg">
+          <h1 className="justify-self-start text-xl ml-5"> EYELADY </h1>
 
-      <InstaViewer />
+          <Link
+            href="/video"
+            className="justify-self-center p-2 border rounded-sm hover:bg-green-600/70 hover:scale-110 transition-transform duration-300"
+          >
+            Videos
+          </Link>
+          <div /* Placeholder column  */ />
+        </div>
+      </div>
+      {/* Top Banner END */}
+
+
+
+      {/* Central Container BEGIN */}
+      <div className="w-full flex justify-center">
+        <div className="grid grid-cols-2 gap-2 max-w-4xl w-full px-4">
+
+          {/* Left Column */}
+          <div className="grid grid-cols-1 gap-2 w-full">
+            <Image
+              src="/anthony.png"
+              width={300}
+              height={300}
+              alt="anthony"
+              className="w-full h-auto border-3 border-cyan-400 rounded-sm p-1"
+            />
+            <div className="border-3 border-cyan-400 rounded-sm p-1">
+              <InstaViewer />
+            </div>
+          </div>
+
+          {/* Right Column */}
+          <div className="grid grid-cols-1 gap-2">
+            <div className="border-3 border-cyan-400 rounded-sm p-1"> <EyeLadyText /> </div>
+            <div className="border-3 border-cyan-400 rounded-sm p-2"> <FrontPageVideo /> </div>
+          </div>
+
+        </div>
+      </div>
+      {/* Central Container END */}
 
     </div>
+
   );
+
 }
