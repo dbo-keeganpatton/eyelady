f53d42b10a200b95d8755748438a6959331383ac
eyelady <keeganpatton@gmail.com>
2025-12-14 16:59:08 -0600

    update theme to cyan | add eyeladytext comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,43 @@
 import InstaViewer from "@/components/igViewer";
+import EyeLadyText from "@/components/eyeladyText";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
+    <div className="flex flex-col w-screen">
+
 
       {/* Top Banner BEGIN */}
-      <div className="bg-pink-500 w-screen text-xl p-1 flex">
-        <h1>EYELADY</h1>
-        <Link
-          href="/video"
-          className="self-end p-1 border rounded-sm hover:bg-green-600/70 hover:scale-110 transition-transform duration-300">
-          <button> Videos </button>
-        </Link>
+      <div className="w-full px-2 pt-2">
+        <div className="grid grid-cols-3 items-center border border-cyan-500 text-md p-1 m-2 rounded-md shadow-lg">
+          <h1 className="justify-self-start text-xl ml-5"> EYELADY </h1>
+
+          <Link
+            href="/video"
+            className="justify-self-center p-2 border rounded-sm hover:bg-green-600/70 hover:scale-110 transition-transform duration-300"
+          >
+            Videos
+          </Link>
+          <div /* Placeholder column  */ />
+        </div>
       </div>
       {/* Top Banner END */}
 
 
-
       {/* Central Container BEGIN */}
-      <div className="flex gap-2">
+      <div className="grid grid-cols-2">
 
         {/* Left Column */}
         <div className="grid grid-cols-1 gap-2 place-items-center">
-          <Image src="/anthony.png" width={300} height={300} alt="anthony" className="border-5 border-green-600/50 rounded-sm p-2" />
-          <div className="border-5 border-green-600/50 rounded-sm p-2"> <InstaViewer /> </div>
+          <Image src="/anthony.png" width={300} height={300} alt="anthony" className="border-3 border-cyan-400 rounded-sm p-1" />
+          <div className="border-3 border-cyan-400 rounded-sm p-1"> <InstaViewer /> </div>
         </div>
 
         {/* Right Column */}
-        <div className="border-5 border-green-600/50 rounded-sm p-2"> <InstaViewer /> </div>
+        <div>
+          <div className="border-3 border-cyan-400 rounded-sm p-1"> <EyeLadyText /> </div>
+        </div>
       </div>
       {/* Central Container END */}
 
---
+++ components/eyeladyText.tsx
@@ -0,0 +1,11 @@
+export default function EyeLadyText() {
+  return (
+    <>
+      <p>
+        If I catch you here again, you&apos;ll have hell to pay. Yes or No? Baby needs smearnaise and slop. Sounding rods coming
+        to a shop near your local.. Fuk lej crawfish back in stock FY28 Q2. Computer techology dissolution has occured, please flush
+        all affected memory. Welcome to the end.
+      </p>
+    </>
+  )
+}
