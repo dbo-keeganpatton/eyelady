621ddaa9df553e610d5e825276aa7bfcb9401855
eyelady <keeganpatton@gmail.com>
2025-12-16 21:53:45 -0600

    Responsive Tile styles

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,16 @@ export default function Home() {
       {/* Top Banner BEGIN */}
       <div className="w-full px-2 pt-2">
         <div className="grid grid-cols-3 items-center border border-blue-500/20 bg-white/5 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg">
-          <h1 className="justify-self-start text-2xl font-mono text-amber-400 ml-5"> EYELADY </h1>
+          <h1 className="justify-self-start text-2xl font-mono text-amber-400 ml-5"> $EYELADY </h1>
 
+
+          <div /* Placeholder column  */ />
           <Link
             href="/video"
-            className="justify-self-center p-2 border rounded-sm hover:bg-green-600/70 hover:scale-110 transition-transform duration-300"
+            className="justify-self-end p-2 m-1 border border-amber-400 shadow-lg text-amber-400 rounded-sm font-mono hover:scale-110 transition-transform duration-300"
           >
             Videos
           </Link>
-          <div /* Placeholder column  */ />
         </div>
       </div>
       {/* Top Banner END */}
@@ -35,10 +36,11 @@ export default function Home() {
           <div className="border-2 border-white/5 h-100"> <FrontPageVideo /> </div>
         </div>
 
-        <div className="grid grid-cols-2 gap-2 mr-10 ml-10">
+        <div className="grid grid-cols-2 gap-1 mr-10 ml-10">
 
           {/* Left Column */}
-          <div className="grid grid-cols-1 gap-2 w-full">
+          <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300"> <EyeLadyText /> </div>
+          <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
             <Image
               src="/anthony.png"
               width={300}
@@ -46,14 +48,14 @@ export default function Home() {
               alt="anthony"
               className="w-full h-auto border-3 border-gray-300 rounded-sm p-1"
             />
-            <div className="border-3 border-gray-300 rounded-sm p-1">
-              <InstaViewer />
-            </div>
           </div>
 
           {/* Right Column */}
-          <div className="grid grid-cols-1 gap-2">
-            <div className="border-3 border-gray-300 rounded-sm p-1"> <EyeLadyText /> </div>
+          <div className="grid grid-cols-1">
+
+            <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
+              <InstaViewer />
+            </div>
           </div>
 
         </div>
