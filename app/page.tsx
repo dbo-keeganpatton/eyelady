a4f9823aae3c81a22ee77e19238ef699c9b9bf1e
eyelady <keeganpatton@gmail.com>
2025-12-16 21:30:12 -0600

    Add glass header

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,9 @@ export default function Home() {
   return (
     <div className="flex flex-col w-screen">
 
-
       {/* Top Banner BEGIN */}
       <div className="w-full px-2 pt-2">
-        <div className="grid grid-cols-3 items-center border border-blue-500 text-md p-1 m-2 rounded-md shadow-lg">
+        <div className="grid grid-cols-3 items-center border border-white/20 bg-white/10 backdrop-blur-xl backdrop-saturate-150 text-md p-1 m-2 rounded-md shadow-lg">
           <h1 className="justify-self-start text-xl ml-5"> EYELADY </h1>
 
           <Link
@@ -31,8 +30,12 @@ export default function Home() {
 
       {/* Central Container BEGIN */}
       <div className="w-full flex flex-col justify-center">
-        <div className="border-3 border-blue-300 rounded-sm p-2 ml-10 mr-10 mt-2 mb-2 h-100"> <FrontPageVideo /> </div>
-        <div className="grid grid-cols-2 gap-2 max-w-4xl w-full px-4">
+
+        <div className="border-4 border-blue-600 ml-11 mr-11 mb-2 mt-2">
+          <div className="border-10 border-white/10 h-100"> <FrontPageVideo /> </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-2 mr-10 ml-10">
 
           {/* Left Column */}
           <div className="grid grid-cols-1 gap-2 w-full">
@@ -41,16 +44,16 @@ export default function Home() {
               width={300}
               height={300}
               alt="anthony"
-              className="w-full h-auto border-3 border-blue-300 rounded-sm p-1"
+              className="w-full h-auto border-3 border-gray-300 rounded-sm p-1"
             />
-            <div className="border-3 border-blue-300 rounded-sm p-1">
+            <div className="border-3 border-gray-300 rounded-sm p-1">
               <InstaViewer />
             </div>
           </div>
 
           {/* Right Column */}
           <div className="grid grid-cols-1 gap-2">
-            <div className="border-3 border-blue-300 rounded-sm p-1"> <EyeLadyText /> </div>
+            <div className="border-3 border-gray-300 rounded-sm p-1"> <EyeLadyText /> </div>
           </div>
 
         </div>
