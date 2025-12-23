cf63700cc42a473a47cdb35f19e61d509d89cf64
eyelady <keeganpatton@gmail.com>
2025-12-22 21:21:50 -0600

    add byron

--- app/page.tsx
+++ app/page.tsx
@@ -15,19 +15,21 @@ export default function Home() {
       **************************************/}
       {/* Top Banner BEGIN */}
       <div>
-        <div className="flex lg:m-4 m-2 border border-blue-500/20 bg-white/5 backdrop-blur-sm backdrop-saturate-140 rounded-sm shadow-lg shadow-neutral-500/20">
-          <h1 className="lg:text-2xl text-sm font-mono text-amber-400 ml-5"> $EYELADY </h1>
+        <div className="flex lg:m-4 m-2 border items-center justify-center border-blue-500/20 bg-white/5 backdrop-blur-sm backdrop-saturate-140 rounded-sm shadow-lg shadow-neutral-500/20">
+          <h1 className="lg:text-2xl text-sm font-mono text-blue-500 ml-5"> $EYELADY </h1>
 
 
-          <div className="h-10 lg:h-18">
-            <a href="https://www.instagram.com/eyeladyskateboards?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-              className="shadow-xl shadow-blue-500"
+          <div className="ml-auto h-10 w-10 lg:h-20 lg:w-20 mr-5 flex items-center justify-center opacity-15">
+            <a href="/Video"
+              className="relative h-20 w-20 flex items-center justify-center 
+               hover:shadow-[0_0_12px_rgba(59,130,246,0.6)]/20
+               rounded-sm"
             >
               <Image
                 src="/video-camera-svgrepo-com.svg"
                 fill={true}
                 alt="instagram"
-                className="hover:scale-110 transition-transform duration-300"
+                className="object-contain hover:scale-110 transition-transform duration-300"
               />
             </a>
           </div>
@@ -41,7 +43,7 @@ export default function Home() {
       {/* Central Container BEGIN */}
       <div className="w-full flex flex-col justify-center">
 
-        <div className="ml-5 mr-5 border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
+        <div className="ml-5 mr-5 border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md rounded-sm shadow-lg hover:border-blue-500/40 hover:scale-101 transition-transform duration-300">
           <div className="aspect-video border-2 border-white/5"> <FrontPageVideo /> </div>
         </div>
 
@@ -56,6 +58,17 @@ export default function Home() {
           <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
             <Image
               src="/anthony.png"
+              width={300}
+              height={300}
+              alt="anthony"
+              className="w-full h-auto border-3 border-gray-300 rounded-sm p-1 opacity-10"
+            />
+          </div>
+
+
+          <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
+            <Image
+              src="/byron.png"
               width={300}
               height={300}
               alt="anthony"
