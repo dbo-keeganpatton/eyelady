b8ba9fd41fff41f55fe92a4a5a78a77b3552ced2
eyelady <keeganpatton@gmail.com>
2026-01-01 21:09:07 -0600

    Stage to align background images with response opac

--- app/page.tsx
+++ app/page.tsx
@@ -43,38 +43,51 @@ export default function Home() {
       {/* Central Container BEGIN */}
       <div className="w-full flex flex-col justify-center">
 
-        <div className="ml-5 mr-5 border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md rounded-sm shadow-lg hover:border-blue-500/40 hover:scale-101 transition-transform duration-300">
-          <div className="aspect-video border-2 border-white/5"> <FrontPageVideo /> </div>
+        <div className="ml-5 mr-5 lg:ml-20 lg:mr-20 border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md rounded-sm shadow-lg hover:border-blue-500/40 hover:scale-101 transition-transform duration-300">
+          <div className="aspect-video border-2 border-white/5"> {/* <FrontPageVideo /> */} </div>
         </div>
 
-        <div className="lg:grid lg:grid-cols-2 gap-1 mr-3 ml-3">
+        <div className="mr-3 ml-3">
 
           {/* Left Column */}
           <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
             <EyeLadyText />
           </div>
 
-          {/* Left Column */}
-          <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
-            <Image
-              src="/anthony.png"
-              width={300}
-              height={300}
-              alt="anthony"
-              className="w-full h-auto border-3 border-gray-300 rounded-sm p-1 opacity-10"
-            />
-          </div>
+          {/* Fixed translucent background images */}
+          <div className="flex">
+            <div className="backdrop-blur-sm backdrop-saturate-100 rounded-sm shadow-lg opacity-10 hover:opacity-100 transition-opacity duration-1000">
+              <Image
+                src="/anthony.png"
+                width={300}
+                height={300}
+                alt="anthony"
+                className="w-full h-auto rounded-sm p-1 opacity-30"
+              />
+            </div>
 
 
-          <div className="border border-blue-500/20 backdrop-blur-sm backdrop-saturate-150 text-md p-1 m-2 rounded-sm shadow-lg hover:border-yellow-500 hover:scale-101 transition-transform duration-300">
-            <Image
-              src="/byron.png"
-              width={300}
-              height={300}
-              alt="anthony"
-              className="w-full h-auto border-3 border-gray-300 rounded-sm p-1 opacity-10"
-            />
+            <div className="backdrop-blur-sm backdrop-saturate-100 rounded-sm shadow-lg opacity-10 hover:opacity-100 transition-opacity duration-1000">
+              <Image
+                src="/byron.png"
+                width={300}
+                height={300}
+                alt="anthony"
+                className="w-full h-auto rounded-sm p-1 opacity-30"
+              />
+            </div>
+
+            <div className="backdrop-blur-sm backdrop-saturate-100 rounded-sm shadow-lg opacity-10 hover:opacity-100 transition-opacity duration-1000">
+              <Image
+                src="/jodey.png"
+                width={300}
+                height={300}
+                alt="anthony"
+                className="w-full h-auto rounded-sm p-1 opacity-30"
+              />
+            </div>
           </div>
+
 
         </div>
       </div>
